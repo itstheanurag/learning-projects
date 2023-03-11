8bd7874cb72c92561885db0fcccf63c70e213650
itstheanurag <gauravanurag36@gmail.com>
2023-03-11 13:06:02 +0530

    fixed tip calculator

--- Tip Calculator/index.js
+++ Tip Calculator/index.js
@@ -7,17 +7,21 @@ pay_tip.addEventListener('input', (event) => {
     if (!bill.value) {
         if (error.classList.contains('hidden')) {
             error.classList.remove('hidden')
-            error.innerHTML = `Must provide value for bill, you entered nothing`
-            console.log(event.target.value)
+            console.log("error1")
         }
 
     } else {
         if (!error.classList.contains('hidden')) {
-            error.classList.add('hidden');
+            error.classList.add('hidden')
+        }
+
+        if (!pay_tip.value) {
+            total_bill.value = "Your total bill will come here"
         }
 
         const percent = parseInt(pay_tip.value);
         const principle = parseInt(bill.value);
+
         if (principle && percent) {
             const bill_to_pay = calculate(principle, percent)
             total_bill.value = (bill_to_pay + principle).toFixed(2)
@@ -35,10 +39,27 @@ bill.addEventListener('input', (event) => {
         error.classList.add('hidden');
     }
 
+    if (typeof bill.value === 'number') {
+        if (!error.classList.contains('hidden')) {
+            error.classList.add('hidden')
+        }
+    }
+
+    if (!bill.value && pay_tip.value) {
+        if (error.classList.contains('hidden')) {
+            error.classList.remove('hidden');
+        }
+
+        total_bill.value = "Your total bill will come here"
+    }
+
     const percent = parseInt(pay_tip.value);
     const principle = parseInt(bill.value);
+
     if (principle && percent) {
         const bill_to_pay = calculate(principle, percent)
         total_bill.value = (bill_to_pay + principle).toFixed(2)
     }
 })
+
+
